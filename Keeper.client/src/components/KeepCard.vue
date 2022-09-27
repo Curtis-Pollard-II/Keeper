ae0faf65a335c5af0f000c034814303e387075d7
Curtis Pollard <clp.the.second@gmail.com>
2022-09-27 12:29:29 -0600

    Tuesday lunch commit

--- Keeper.client/src/components/KeepCard.vue
+++ Keeper.client/src/components/KeepCard.vue
@@ -1,10 +1,17 @@
 <template>
-
-    <div class="p-3">
-        <div class="bg-danger text-center p-4">
-            <img src="keep.profile.picture" alt="">
-            <h3 class="m-0 p-3"></h3>
+<!-- add router link here -->
+    <div class=" container-fluid p-3">
+        <div class="bg-light border p-1 elevation-2 rounded">
+            <img class="img-fluid" :src="keep.img" alt="image goes here" style="width:100%;">
+            <div>
+                <h3 class="text-white  content with-eight">{{keep.name}}
+                    <div class="righty">
+                        <img :src="keep.creator.picture"/>
+                    </div>
+                </h3>
+            </div>    
         </div>
+
     </div>
 
 
@@ -14,6 +21,7 @@
 
 <script>
 export default {
+    props: { keep: {type: Object, required: true }},
 setup() {
   return {};
 },
@@ -22,5 +30,43 @@ setup() {
 
 
 <style> 
+.container-fluid {
+    position: relative;
+    font-family: "Courier Bold"; 
+    font-size: 24px; 
+    font-style: normal; 
+    font-variant: normal; 
+    font-weight: 700; 
+    line-height: 26.4px; }
+
+
+.container-fluid .content {
+    
+    position: absolute; /* Position the background text */
+    bottom: 0; /* At the bottom. Use top:0 to append it to the top */
+    left: 10px;
+    width: 100%; /* Full width */
+    padding: 20px; /* Some padding */
+}
+
+.with-eight {
+    text-shadow:
+        0.05em 0 black,
+        0 0.05em black,
+        -0.05em 0 black,
+        0 -0.05em black,
+        -0.05em -0.05em black,
+        -0.05em 0.05em black,
+        0.05em -0.05em black,
+        0.05em 0.05em black;
+}
+
+
+.righty{
+    position: absolute;
+    bottom: 13px;
+    right: 36px;
+}
+
 
 </style>
--- Keeper.client/src/pages/HomePage.vue
+++ Keeper.client/src/pages/HomePage.vue
@@ -1,10 +1,16 @@
 <template>
-
+ <div class="masonry">
+    <div v-for="k in keeps" :key="k.id">
+      <KeepCard :keep="k" />
+    </div>
+</div>
+  
 </template>
 
 <script>
 import { computed, onMounted } from '@vue/runtime-core';
 import { AppState } from '../AppState';
+import KeepCard from '../components/KeepCard.vue';
 import { logger } from '../utils/Logger';
 import { keepsService } from '../services/KeepsService'
 import Pop from '../utils/Pop';
@@ -24,12 +30,20 @@ export default {
     });
     return {
       keeps: computed(() => AppState.keeps)
-    }
-  }
+    };
+  },
+  components: { KeepCard }
 }
 </script>
 
 <style scoped lang="scss">
+.masonry {
+  columns: 325px;
+  column-gap: .5em;
 
-
+  div {
+    display: block;
+    margin-bottom: .5em;
+  }
+}
 </style>
